7f806e6c2ebe4e30a65478810f9304ac000c9bf7
kingsalah94 <salahou.mohamed@est.edu.ne>
2025-07-28 11:50:19 +0100

    Creation du Projet

---
+++ src/test/java/com/slahtech/BarberShop_Apis/BarberShopApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slahtech.BarberShop_Apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
